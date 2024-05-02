40d216c6817ab566d491a1c7b40e2acb0faf8184
Ahmed Osama <ahmed.osaama10@gmail.com>
2024-05-03 00:38:22 +0200

    First Commit

---
+++ app/user/UserProfile.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { User } from '@supabase/supabase-js';
+import { supabase } from '@/utils/supabaseClient';
+import LoginForm from './LoginForm';
+
+export default function UserProfile() {
+  const [user, setUser] = useState<User | null>(null);
+  const [stripeCustomer, setStripeCustomer] = useState<any>(null);
+
+  useEffect(function getUser() {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+      if (user) {
+        const { data: stripeCustomerData, error } = await supabase
+          .from('stripe_customers')
+          .select('*')
+          .eq('user_id', user.id)
+          .single();
+
+        if (error) {
+          console.log('No stripe customer data found');
+        } else {
+          setStripeCustomer(stripeCustomerData);
+        }
+      }
+    };
+    fetchUser();
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        if (session) {
+          setUser(session.user);
+        }
+      } else if (event === 'SIGNED_OUT') {
+        setUser(null);
+        setStripeCustomer(null);
+      }
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <p>
+            Signed in with email: <strong>{user.email}</strong>
+          </p>
+          <p>
+            Supabase User ID: <strong>{user.id}</strong>
+          </p>
+          <div>
+            <button className="btn btn-secondary btn-sm my-3" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+
+          <h2>Stripe Customer Data</h2>
+          {stripeCustomer ? (
+            <>
+              <p>This data lives in the stripe_customers table in Supabase</p>
+              <div className="mockup-code">
+                <pre>
+                  <code>{JSON.stringify(stripeCustomer, null, 2)}</code>
+                </pre>
+              </div>
+            </>
+          ) : (
+            <div>
+              <p className="text-yellow-500">Stripe customer data not created yet. Buy a plan!</p>
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <p>No user logged in.</p>
+          <LoginForm />
+        </>
+      )}
+    </div>
+  );
+}
---
+++ app/user/page.tsx
@@ -0,0 +1,10 @@
+import UserProfile from './UserProfile';
+const page = () => {
+  return (
+    <section>
+      <h1>User Page</h1>
+      <UserProfile />
+    </section>
+  );
+};
+export default page;
